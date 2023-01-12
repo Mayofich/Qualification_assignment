4fa3f2fddac2409fa7401ff87c24a3d1627a237a
MajaS <maja.skripelj@outlook.com>
2023-01-12 22:27:03 +0100

    more debugging

--- Qualification_API_Blazor/Qualification_API_Blazor/Services/StrojService.cs
+++ Qualification_API_Blazor/Qualification_API_Blazor/Services/StrojService.cs
@@ -1,4 +1,6 @@
 ﻿using Qualification_API_Blazor.Models;
+using Dapper;
+
 namespace Qualification_API_Blazor.Services
 {
     public class StrojService : IStrojService
@@ -12,25 +14,26 @@ namespace Qualification_API_Blazor.Services
 
         public async Task<bool> CreateStroj(Stroj stroj)
         {
-            var result = await _dbService.Insert<int>("INSERT INTO public.Stroj (id,name, age, address, mobile_number) VALUES (@Id, @Name, @Age, @Address, @MobileNumber)", stroj);
+            var result = await _dbService.Insert<int>("STROJEVI", stroj);
             return true;
         }
 
         public async Task<List<Stroj>> GetStrojList()
         {
             var StrojList = await _dbService.GetAll<Stroj>("SELECT * FROM public.Stroj", new { });
-            return StrojList;
+			Console.WriteLine("radi");
+			return StrojList;
         }
 
         public async Task<Stroj> UpdateStroj(Stroj stroj)
         {
-            var updateStroj = await _dbService.Update<int>("Update public.Stroj SET name=@Name, age=@Age, address=@Address, mobile_number=@MobileNumber WHERE id=@Id", stroj);
+            var updateStroj = await _dbService.Update<int>("Update public.Stroj SET NAZIV_STROJA=@NAZIV_STROJA WHERE ID=@ID", stroj);
             return stroj;
         }
 
         public async Task<bool> DeleteStroj(int key)
         {
-            var deleteStroj = await _dbService.Delete<int>("DELETE FROM public.Stroj WHERE id=@Id", new { id = key });
+            var deleteStroj = await _dbService.Delete<int>("DELETE FROM public.Stroj WHERE ID=@ID", new { ID = key });
             return true;
         }
     }
